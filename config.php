45b0d1cc94f1427fe5fc3a61e7dcfc7e007cd4b8
SuDa-20140515XF\Administrator <403060825@qq.com>
2017-08-05 15:27:02 +0800

    new

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+    //'配置项'=>'配置值'
+//    'DB_TYPE' => 'mysql',
+//    'DB_HOST' => 'localhost', // 鏈嶅姟鍣ㄥ湴鍧�
+//    'DB_NAME' => 'boke_db', // 鏁版嵁搴撳悕
+//    'DB_USER' => 'root', // 鐢ㄦ埛鍚?
+//    'DB_PORT'               =>  '3306',        // 绔彛
+//    'DB_PWD' => '', // 瀵嗙爜
+//    'DB_FIELDTYPE_CHECK'    =>  false,       // 鏄惁杩涜瀛楁绫诲瀷妫�鏌?
+//    'DB_CHARSET'            =>  'utf8',      // 鏁版嵁搴撶紪鐮侀粯璁ら噰鐢╱tf8
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '139.199.169.106', // 鏈嶅姟鍣ㄥ湴鍧�
+    'DB_NAME' => 'kongcheng_base', // 鏁版嵁搴撳悕
+    'DB_USER' => 'root', // 鐢ㄦ埛鍚?
+    'DB_PWD' => 'q126654803c', // 瀵嗙爜
+    'DB_PORT'               =>  '3306',        // 绔彛
+    'DB_FIELDTYPE_CHECK'    =>  false,       // 鏄惁杩涜瀛楁绫诲瀷妫�鏌?
+    'DB_CHARSET'            =>  'utf8',      // 鏁版嵁搴撶紪鐮侀粯璁ら噰鐢╱tf8
+
+);
